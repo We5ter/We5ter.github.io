4b88eca77a3180b72b22099b38c8034d1f56e899
Wester <zyzengstorm@gmail.com>
2021-02-19 17:35:40 +0800

    Update poc.js

--- js/poc/poc.js
+++ js/poc/poc.js
@@ -23,7 +23,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
-      location="https://www.google.com";
+      location="https://www.google.com:2333";
       if (choiceResult.outcome === 'accepted') {
         console.log('User accepted the A2HS prompt');
       } else {
