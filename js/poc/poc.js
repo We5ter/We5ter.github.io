1aeb7353fcd51841dda7034cb23dfe6457ac8576
Wester <zyzengstorm@gmail.com>
2021-02-19 17:23:47 +0800

    Update poc.js

--- js/poc/poc.js
+++ js/poc/poc.js
@@ -19,7 +19,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
   addBtn.addEventListener('click', () => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
-    prompt('111')
+    document.location.href="https://www.google.com"
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
