fea3dc791a08b01c64b8d62aaac56179b4eff54e
Wester <zyzengstorm@gmail.com>
2021-03-17 21:06:16 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5140,7 +5140,7 @@ default = s
 						href: "https://github.com/monsterzer0",
 						target: "_blank"
 					}
-				}, ["Monster Zero Team"]), " and UESTC's StarStudio."]), e("br"), e("h1", ["Focused on Browser Security & Pentest & Applied Security."]), e("br"), e("h1", ["Also interested on new techniques like AI."]), e("br"), e("h1", ["Photog, violin, swim and cooking are all on my timetable."])])
+				}, ["Monster Zero Team"]), " and UESTC's StarStudio."]), e("br"), e("h1", ["Focused on Browser Security & Pentest & Applied Security."]), e("br"), e("h1", ["Also interested on new techniques like AI-GAN and AI-Fuzz."]), e("br"), e("h1", ["Photog, violin, swim and cooking are all on my timetable."])])
 			}
 		}]), t
 	}();
