67e55b915cd84c7bc587fa3a02ca07760ee96bdc
Wester <zyzengstorm@gmail.com>
2021-06-11 17:10:32 +0800

    Update poc.js

--- js/poc/poc.js
+++ js/poc/poc.js
@@ -9,7 +9,6 @@ const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-		  window.open("https://www.apple.com");
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
